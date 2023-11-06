624bdd3aaa30fb3db11c8a7db7191571c1d1fff1
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2023-11-06 06:46:37 +0600

    added functional search option and sort button in available food and featured food shows the most quantity of foodorder

--- src/pages/AvailableFood/AvailableFood.jsx
+++ src/pages/AvailableFood/AvailableFood.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import AvailableFoodCard from "./AvailableFoodCard";
 
 const AvailableFood = () => {
+  const [availableFoods, setAvailableFoods] = useState([]);
+  const [searchFood, setSearchFood] = useState("");
+  const [sortedFoods, setSortedFoods] = useState(availableFoods);
+
+  useEffect(() => {
+    fetch("featuredFoods.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setAvailableFoods(data);
+        setSortedFoods(data);
+      });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchFood(event.target.value);
+    const filteredFoods = availableFoods.filter((food) =>
+      food.foodName.toLowerCase().includes(event.target.value.toLowerCase()) // Adjust the field name
+    );
+    setSortedFoods(filteredFoods);
+  };
+
+  const handleSort = () => {
+    const sortedFoodsCopy = [...sortedFoods];
+    sortedFoodsCopy.sort((a, b) => new Date(a.expiredDate) - new Date(b.expiredDate)); // Adjust the field name
+    setSortedFoods(sortedFoodsCopy);
+  };
+
   return (
     <div className="my-10 space-y-16 px-4">
       <div
@@ -10,6 +39,28 @@ const AvailableFood = () => {
           <div className="pt-32 pl-20">
             <h3 className="text-5xl font-bold">Available Food</h3>
           </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-between gap-4 items-center px-8 mb-4">
+          <input
+            type="text"
+            placeholder="Search by Food Name"
+            value={searchFood}
+            onChange={handleSearch}
+            className="p-2 border-2 border-red-600 text-black rounded"
+          />
+          <button onClick={handleSort} className="px-4 py-2 bg-red-500 text-white rounded">
+            Sort by Expire Date
+          </button>
+        </div>
+        <div className="px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
+          {sortedFoods.map((availableFood) => (
+            <AvailableFoodCard
+              key={availableFood.id}
+              availableFood={availableFood}
+            ></AvailableFoodCard>
+          ))}
         </div>
       </div>
     </div>
--- src/pages/Home/FeaturedFoods/FeaturedFoods.jsx
+++ src/pages/Home/FeaturedFoods/FeaturedFoods.jsx
@@ -7,7 +7,13 @@ const FeaturedFoods = () => {
   useEffect(() => {
     fetch("featuredFoods.json")
       .then((res) => res.json())
-      .then((data) => setFeaturedFoods(data));
+      .then((data) => {
+        const sortedFeaturedFoods = data.sort(
+          (a, b) => b.foodQuantity - a.foodQuantity
+        );
+        const top6FeaturedFoods = sortedFeaturedFoods.slice(0, 6);
+        setFeaturedFoods(top6FeaturedFoods);
+      });
   }, []);
 
   return (
@@ -15,7 +21,8 @@ const FeaturedFoods = () => {
       <div className="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10">
         <h4 className="text-xl text-red-500 font-bold mb-6">Featured Foods</h4>
         <h2 className="text-5xl font-bold mb-8 text-center">
-        Featured Foods represent the <br />highest quantity of food options available.
+          Featured Foods represent the <br />
+          highest quantity of food options available.
         </h2>
         <div className="px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
           {featuredFoods.map((featuredFood) => (
@@ -25,7 +32,7 @@ const FeaturedFoods = () => {
             ></FeaturedFoodCard>
           ))}
         </div>
-        <Link to="/available-foods" >
+        <Link to="/available-foods">
           <button className="px-8 py-2 bg-red-500 font-semibold rounded hover:bg-red-800 hover:text-white dark:bg-red-500 dark:text-black">
             Show All
           </button>
